e41f9f59b2c57208470377e5afd21d23a7c3c69c
Ayaka Ito <a.8.32.noko.info@gmail.com>
2025-03-23 19:10:31 -0700

    file size limit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,8 +34,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Routes
 app.use("/users", userRouter);
